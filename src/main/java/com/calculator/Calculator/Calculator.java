ef44295790e0fd9d7cb1e725d224b8a6a6f44ff1
urjasrivastava <urjasrivastava123@gmail.com>
2021-03-08 15:37:50 +0530

    added playbook.yml

--- src/main/java/com/calculator/Calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator/Calculator.java
@@ -8,5 +8,24 @@ public class Calculator
     {
         return (int) Math.pow(a,b);
     }
+    double sqrt(double a)
+    {
+        return Math.sqrt(a);
+    }
+    double log(double a)
+    {
+        return Math.log(a);
+    }
+    long factorial(int a)
+    {
+        if(a<0)
+            return 0;
+        if(a==0)
+            return 1;
+        long fact=1;
+        for(int i=1;i<=a;i++)
+            fact*=i;
+        return fact;
+    }
 }
 
--- src/main/java/com/calculator/Calculator/CalculatorController.java
+++ src/main/java/com/calculator/Calculator/CalculatorController.java
@@ -12,7 +12,30 @@ class CalculatorController
     @RequestMapping(value="/pow", method= RequestMethod.GET)
     public String pow(@RequestParam("a") Integer a, @RequestParam("b") Integer b,Model model)
     {
+        String display=String.format("pow(%d,%d)",a,b);
+        model.addAttribute("tag",String.valueOf(display));
         model.addAttribute("result",String.valueOf(calculator.pow(a,b)));
+        return "answer";
+    }
+    @RequestMapping(value="/log", method= RequestMethod.GET)
+    public String log(@RequestParam("a") Double a, Model model)
+    {   String display=String.format("log(%f)",a);
+        model.addAttribute("tag",String.valueOf(display));
+        model.addAttribute("result",String.valueOf(calculator.log(a)));
+        return "answer";
+    }
+    @RequestMapping(value="/sqrt", method= RequestMethod.GET)
+    public String sqrt(@RequestParam("a") Double a,Model model)
+    {   String display=String.format("sqrt(%f)",a);
+        model.addAttribute("tag",String.valueOf(display));
+        model.addAttribute("result",String.valueOf(calculator.sqrt(a)));
+        return "answer";
+    }
+    @RequestMapping(value="/factorial", method= RequestMethod.GET)
+    public String factorial(@RequestParam("a") Integer a,Model model)
+    {   String display=String.format("factorial(%d)",a);
+        model.addAttribute("tag",String.valueOf(display));
+        model.addAttribute("result",String.valueOf(calculator.factorial(a)));
         return "answer";
     }
 }
